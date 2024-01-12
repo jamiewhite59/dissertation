57c75245782203b899ee40f9ba0b68065fcaa453
Jamie White <jamiewhite59@outlook.com>
2024-01-12 15:32:48 +0000

    Create basic events route

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -2,9 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Event;
+use Carbon\Carbon;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Inertia\Response;
 
 class EventController extends Controller
 {
-    //
+    public function show(Request $request): Response {
+        return Inertia::render('Events/Landing', [
+            'name' => 'Jamie'
+        ]);
+    }
+
+    public function create(Request $request): RedirectResponse {
+        $event = new Event;
+        $event->title = 'Test Event 1';
+        $event->start_date = Carbon::today();
+        $event->end_date = Carbon::today();
+        $event->icon = 'fa-plus';
+
+        $event->save();
+
+        return redirect('/events');
+    }
 }
