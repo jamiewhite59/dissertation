a2f1ceb9ee22dc7994bd3d4e3642cc8c17a1c0cc
Jamie White <jamiewhite59@outlook.com>
2024-01-16 20:04:28 +0000

    Create initial main layout

---
+++ resources/js/Components/sidebar.vue
@@ -0,0 +1,34 @@
+<script>
+// import {Setting, Location} from '@element-plus/icons-vue';
+
+export default {
+// components: {
+// 	Setting,
+// 	Location,
+// }
+}
+</script>
+
+<template>
+	<el-menu>
+		<el-menu-item index="1">
+			<!-- <el-icon><Setting /></el-icon> -->
+			<span>Events</span>
+		</el-menu-item>
+		<el-menu-item index="2">
+			<span>Customers</span>
+		</el-menu-item>
+		<el-menu-item index="3">
+			<span>Items</span>
+		</el-menu-item>
+		<el-menu-item index="4">
+			<span>Organisation</span>
+		</el-menu-item>
+		<el-menu-item index="5">
+			<span>Logs</span>
+		</el-menu-item>
+		<el-menu-item index="6">
+			<span>User</span>
+		</el-menu-item>
+	</el-menu>
+</template>
---
+++ resources/js/Layouts/MainLayout.vue
@@ -0,0 +1,24 @@
+<script>
+import sidebar from '@/Components/sidebar.vue';
+
+export default {
+	components: {
+		sidebar,
+	},
+	props: {
+		title: String,
+	}
+}
+</script>
+
+<template>
+	<div class="main-layout">
+	  <el-container>
+		<el-aside width="200px"><sidebar></sidebar></el-aside>
+		<el-container>
+		  <el-header>{{title}}</el-header>
+		  <el-main>Main</el-main>
+		</el-container>
+	  </el-container>
+	</div>
+  </template>
