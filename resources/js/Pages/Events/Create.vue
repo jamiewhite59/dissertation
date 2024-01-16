e77893489b1cb0e5c3e673a23f0dbdb5f1ec6cde
Jamie White <jamiewhite59@outlook.com>
2024-01-16 20:04:48 +0000

    Create landing and create vue files.

---
+++ resources/js/Pages/Events/Create.vue
@@ -0,0 +1,7 @@
+<script setup>
+
+</script>
+
+<template>
+	<div>This is the create event page with a form and a create button</div>
+</template>
---
+++ resources/js/Pages/Events/Landing.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { Head } from '@inertiajs/vue3';
+
+import MainLayout from '@/Layouts/MainLayout.vue';
+
+const props = defineProps(['name']);
+
+function create() {
+	console.debug('this is a test ');
+}
+</script>
+
+<template>
+    <Head title="Events" />
+
+	<MainLayout title="Hello there">
+
+	</MainLayout>
+	<!-- <h2>
+		Hi there
+	</h2>
+	<div>
+		Hellooooooo
+		<p>{{ name }}</p>
+		<el-button type="primary" :icon="Plus" @click="create">Create Event</el-button>
+	</div> -->
+
+</template>
