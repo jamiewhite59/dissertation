dba6e8e9b5289426e0b3a250c056a8f24c4baeaf
Jamie White <jamiewhite59@outlook.com>
2024-01-20 17:07:18 +0000

    Adding back sidebar functionality without ziggy

--- resources/js/Components/sidebar.vue
+++ resources/js/Components/sidebar.vue
@@ -7,44 +7,40 @@ export default {
 	},
 	methods: {
 		openEvents() {
-			router.get(route('events.index'));
+			router.visit(route('events.index'));
 		},
 		openCustomers() {
-			router.get(route('customers.index'));
+			router.visit(route('customers.index'));
 		},
 	},
 };
 </script>
 
 <template>
-	<el-container direction="vertical" class="sidebar">
-		<el-button @click="openCustomers">Customers</el-button>
-		<el-button @click="openEvents">Events</el-button>
-	</el-container>
-	<!-- <el-menu>
-		<el-menu-item index="1" @click="route('customers')">
-			<el-icon><Postcard /></el-icon>
+	<el-menu>
+		<el-menu-item index="1" @click="openEvents()">
+			<!-- <el-icon><Postcard /></el-icon> -->
 			<span>Events</span>
 		</el-menu-item>
-		<el-menu-item index="2" @click="customers()">
-			<el-icon><Coordinate /></el-icon>
+		<el-menu-item index="2" @click="openCustomers()">
+			<!-- <el-icon><Coordinate /></el-icon> -->
 			<span>Customers</span>
 		</el-menu-item>
 		<el-menu-item index="3" disabled>
-			<el-icon><Document /></el-icon>
+			<!-- <el-icon><Document /></el-icon> -->
 			<span>Items</span>
 		</el-menu-item>
 		<el-menu-item index="4" disabled>
-			<el-icon><OfficeBuilding /></el-icon>
+			<!-- <el-icon><OfficeBuilding /></el-icon> -->
 			<span>Organisation</span>
 		</el-menu-item>
 		<el-menu-item index="5" disabled>
-			<el-icon><Files /></el-icon>
+			<!-- <el-icon><Files /></el-icon> -->
 			<span>Logs</span>
 		</el-menu-item>
 		<el-menu-item index="6" disabled>
-			<el-icon><User /></el-icon>
+			<!-- <el-icon><User /></el-icon> -->
 			<span>User</span>
 		</el-menu-item>
-	</el-menu> -->
+	</el-menu>
 </template>
