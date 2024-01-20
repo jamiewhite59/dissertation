785e8a8dfbd6135eb2af1d90040f075480642419
Jamie White <jamiewhite59@outlook.com>
2024-01-20 17:06:49 +0000

    Removing Ziggy from app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,7 +4,6 @@ import '../css/app.css';
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
 
 import ElementPlus from 'element-plus';
 import 'element-plus/theme-chalk/src/dark/var.scss';
@@ -18,9 +17,7 @@ createInertiaApp({
 	setup({ el, App, props, plugin, }) {
 		return createApp({ render: () => h(App, props), })
 			.use(plugin)
-			.use(ZiggyVue)
 			.use(ElementPlus)
-		// .mixin({methods: {appRouteMethod: route }})
 			.mount(el);
 	},
 	progress: {
