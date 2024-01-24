bbf9a27ab6ada2648555ab21f468c86a5f4fdb70
Jamie White <jamiewhite59@outlook.com>
2024-01-24 17:57:54 +0000

    Factory/Seeder for Event

---
+++ database/factories/EventFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Event>
+ */
+class EventFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'id' => Str::uuid()->toString(),
+            'title' => fake()->country(),
+            'start_date' => fake()->dateTimeBetween('-1 week', '+2 weeks'),
+        ];
+    }
+}
