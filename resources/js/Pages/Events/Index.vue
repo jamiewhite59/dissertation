3f0995c8e3df3df4f06c7084540d47fbd4e6c073
Jamie White <jamiewhite59@outlook.com>
2024-01-24 17:02:05 +0000

    Events index page layout

--- resources/js/Pages/Events/Index.vue
+++ resources/js/Pages/Events/Index.vue
@@ -1,25 +1,87 @@
 <script>
 import MainLayout from '@/Layouts/MainLayout.vue';
+import { Plus } from '@element-plus/icons-vue';
+import { router } from '@inertiajs/vue3';
 
 export default {
 	components: {
 		MainLayout,
+		Plus,
 	},
 	props: {
-		name: String,
+		events: Array,
 	},
-
+	data() {
+		return {
+			search: '',
+		};
+	},
 	methods: {
-		create() {
-			console.debug('this is a test');
+		openCreate() {
+			router.get(route('events.create'));
+		},
+		openEdit(id) {
+			router.get(route('events.edit', id));
 		},
 	},
 };
-
 </script>
 
 <template>
 	<MainLayout title="Events">
-		<div>Events Landing</div>
+		<el-container>
+			<el-header class="event-index-create">
+				<span>Search</span>
+				<el-input v-model="search" placeholder="Search Events" clearable style="width:450px"/>
+				<el-button type="primary" @click="openCreate">
+					<template #icon>
+						<el-icon><Plus/></el-icon>
+					</template>
+					Create
+				</el-button>
+			</el-header>
+			<el-main class="event-index-list">
+				<el-container class="list-space">
+					<el-card class="list-card" v-for="event in events" :key="event.id" shadow="hover" @click="openEdit(event.id)">
+						<el-descriptions :title="event.title" :column="1">
+							<el-descriptions-item>Some event information</el-descriptions-item>
+						</el-descriptions>
+					</el-card>
+				</el-container>
+			</el-main>
+		</el-container>
 	</MainLayout>
 </template>
+<style lang="scss">
+.event-index-create {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	gap: 1em;
+}
+
+.event-index-list {
+	flex: initial !important;
+
+	.list-space {
+		display: grid !important;
+		grid-gap: 15px;
+		grid-template-columns: repeat(auto-fill, minmax(220px, 1fr));
+
+		.space-item__item {
+			width: 24%;
+		}
+
+		.list-card {
+			text-align: center;
+			height: 120px;
+		}
+
+		.el-card.is-hover-shadow:hover{
+			border: 1px solid var(--el-color-primary);
+			cursor: pointer;
+		}
+	}
+
+}
+</style>
