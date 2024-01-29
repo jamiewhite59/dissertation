9582d02e4e91af8268e8cb346b6992e6bbbde7be
Jamie White <jamiewhite59@outlook.com>
2024-01-29 09:48:20 +0000

    Basic layout for items index page.

--- resources/js/Pages/Items/Index.vue
+++ resources/js/Pages/Items/Index.vue
@@ -1,15 +1,28 @@
 <script>
+import { router } from '@inertiajs/vue3';
+
 export default {
 	data() {
 		return {
+			search: '',
 		};
 	},
 	methods: {
+		openCreate() {
+			router.get(route('items.create'));
+		},
 	},
 };
 </script>
 <template>
-	<MainLayout title="Items"/>
+	<MainLayout title="Items">
+		<OverviewLayout @openCreate="openCreate">
+			<template #search>
+				<el-input v-model="search" placeholder="Search Items" clearable style="width:450px"/>
+			</template>
+			<template #default>Test content</template>
+		</OverviewLayout>
+	</MainLayout>
 </template>
 <style lang="scss">
 
