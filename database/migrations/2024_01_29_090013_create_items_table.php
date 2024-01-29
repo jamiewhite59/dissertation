44b87921732e45359e433d220ca253d55c234eb8
Jamie White <jamiewhite59@outlook.com>
2024-01-29 09:13:14 +0000

    Create Item controller & table migration.

---
+++ database/migrations/2024_01_29_090013_create_items_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->uuid('id');
+            $table->string('title');
+            $table->string('description')->nullable();
+            $table->binary('image')->nullable();
+            $table->enum('stock_type', ['bulk', 'hire'])->default('bulk');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('items');
+    }
+};
