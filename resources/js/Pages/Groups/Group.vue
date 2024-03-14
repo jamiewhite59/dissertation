e5d3ccc59ad45a057855205579a00093abfce750
Jamie White <jamiewhite59@outlook.com>
2024-03-14 12:06:46 +0000

    Pressing enter on form adds pieces to a group

--- resources/js/Pages/Groups/Group.vue
+++ resources/js/Pages/Groups/Group.vue
@@ -1,6 +1,6 @@
 <script>
 import { router } from '@inertiajs/vue3';
-
+import { reactive } from 'vue';
 import { ElMessageBox } from 'element-plus';
 
 export default {
@@ -13,7 +13,14 @@ export default {
 		return {
 			formChanges: false,
 			pieceDialogVisible: false,
-			pieceCodeInput: '',
+			pieceForm: reactive({
+				pieceCode: '',
+			}),
+			rules: reactive({
+				pieceCode: [
+					{ required: true, message: 'Code is required', trigger: 'blur', },
+				],
+			}),
 		};
 	},
 	methods: {
@@ -36,9 +43,24 @@ export default {
 		save() {
 			this.$refs.groupForm.save();
 		},
+		validatePieceForm() {
+			return this.$refs.pieceForm.validate()
+				.then((valid) => {
+					return true;
+				})
+				.catch((err) => {
+					return false;
+				});
+		},
 		addPiece() {
-			router.put(route('groups.addPiece', this.group.id), {piece_code: this.pieceCodeInput,});
-			this.pieceCodeInput = '';
+			this.validatePieceForm()
+				.then((valid) => {
+					if (valid) {
+						router.put(route('groups.addPiece', this.group.id), {piece_code: this.pieceForm.pieceCode,});
+						this.pieceForm.pieceCode = '';
+					}
+					this.$refs.pieceSearch.focus();
+				});
 		},
 		removePiece(id) {
 			ElMessageBox.confirm(
@@ -55,6 +77,11 @@ export default {
 		},
 		openItem(piece) {
 			router.get((route('items.edit', piece.item.id)));
+		},
+		hideDialog() {
+			this.pieceDialogVisible = false;
+			this.pieceForm.piece_code = '';
+			this.$refs.pieceForm.resetFields();
 		},
 	},
 };
@@ -83,14 +110,21 @@ export default {
 			</template>
 		</CreateLayout>
 	</MainLayout>
-	<el-dialog v-model="pieceDialogVisible" width="30%" style="min-height:400px;" align-center @opened="$refs.pieceSearch.focus()">
+	<el-dialog v-model="pieceDialogVisible" width="30%" style="min-height:150px;" align-center @opened="$refs.pieceSearch.focus()" @closed="hideDialog">
 		<template #header>Add Piece</template>
 		<template #default>
-			<el-input v-model="pieceCodeInput" ref="pieceSearch" placeholder="Enter Piece Code" clearable @keyup.enter="addPiece">
-				<template #append>
-					<el-button @click="addPiece">Add</el-button>
-				</template>
-			</el-input>
+			<el-form ref="pieceForm" label-position="top" :model="pieceForm" :rules="rules" @keyup.enter="addPiece">
+				<el-form-item label="Piece Code" prop="pieceCode" required>
+					<el-input v-model="pieceForm.pieceCode" ref="pieceSearch" placeholder="Enter Piece Code" clearable >
+						<template #append>
+							<el-button @click="addPiece">Add</el-button>
+						</template>
+					</el-input>
+				</el-form-item>
+				<el-form-item style="display:none">
+					<el-input/>
+				</el-form-item>
+			</el-form>
 		</template>
 	</el-dialog>
 </template>
